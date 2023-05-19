80a0ef03aef1d92c03bbc5529dd0303b28664aa0
antony-lucide <antony.lucide@laplateforme.io>
2023-05-19 15:10:24 +0200

    Ceci est le rendu officiel du jour09 de la runtrack php.

---
+++ Jour09/Job02/Job2.sql
@@ -0,0 +1,105 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: Jour09
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.20.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `etage`
+--
+
+DROP TABLE IF EXISTS `etage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `etage` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `numero` int DEFAULT NULL,
+  `superficie` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etage`
+--
+
+LOCK TABLES `etage` WRITE;
+/*!40000 ALTER TABLE `etage` DISABLE KEYS */;
+/*!40000 ALTER TABLE `etage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `etudiant`
+--
+
+DROP TABLE IF EXISTS `etudiant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `etudiant` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `naissance` date DEFAULT NULL,
+  `sexe` varchar(25) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `etudiant`
+--
+
+LOCK TABLES `etudiant` WRITE;
+/*!40000 ALTER TABLE `etudiant` DISABLE KEYS */;
+INSERT INTO `etudiant` VALUES (1,'Cyril','Zimmermann','1989-01-02','Homme','cyril@laplateforme.io'),(2,'Jessica','Soriano','1995-09-08','Femme','Jessica@laplateforme.io'),(3,'Roxan','Roumégas','2016-09-08','Homme','Roxan@laplateforme.io'),(4,'Pascal','Assens','1999-12-31','Homme','Pascal@laplateforme.io'),(5,'Terry','Cristinelli','2005-02-01','Homme','Terry@laplateforme.io'),(6,'Ruben','Habib','1993-05-26','Homme','ruben.habib@laplateforme.io'),(7,'Toto','Dupont','2019-11-07','Homme','toto@laplateforme.io');
+/*!40000 ALTER TABLE `etudiant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `salles`
+--
+
+DROP TABLE IF EXISTS `salles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `salles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) DEFAULT NULL,
+  `id_etage` int DEFAULT NULL,
+  `capacite` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `salles`
+--
+
+LOCK TABLES `salles` WRITE;
+/*!40000 ALTER TABLE `salles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `salles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-19 14:05:20
